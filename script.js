394e4cf5427ebf1bd41525cef9d4a5a6f7b14847
siddhisonii <siddhisoni9335@gmail.com>
2024-01-28 15:06:48 +0530

    page 1 done

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-
-
-var h5Timer=document.querySelector(".line h5")
-var grow=0;
-setInterval(()=>{
-     if(grow<100){
-        h5Timer.innerHTML= grow++;
-      console.log(grow);
-     }
-     else{
-        h5Timer.innerHTML= grow;
-     }
-},40)
-
-
+function loadingAnimation(){
 var tl=gsap.timeline()
 
 tl.from(".line h1",{
@@ -23,8 +9,21 @@ tl.from(".line h1",{
 })
 
 tl.from("#line1-part1",{
-    opacity:0
-})
+    opacity:0,
+    onStart: function () {
+        var h5Timer = document.querySelector("#line1-part1 h5");
+        var grow = 0;
+        setInterval(function () {
+          if (grow < 100) {
+            h5Timer.innerHTML = grow++;
+          } else {
+            h5Timer.innerHTML = grow;
+          }
+        }, 33);
+      },
+});
+
+
 
 tl.to(".line h2",{
 animationName:"anime",
@@ -35,8 +34,10 @@ opacity:1
 tl.to("#loader",{
 opacity:0,
 duration:0.2,
-delay:4.7
+delay:4
 })
+
+
 
 tl.from("#page1",{
   y:1600,
@@ -49,3 +50,31 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav",{
+  opacity:0
+ })
+
+tl.from("#hero1 h1 , #hero2 h1 , #hero3 h2, #hero4 h1",{
+  y:120,
+  stagger:0.2
+})
+
+
+}
+
+function cursorAnimation(){
+  document.addEventListener("mousemove",function(dets){
+    gsap.to(".crsr",{
+      left:dets.x,
+      top:dets.y
+    })
+  })
+  
+  Shery.makeMagnet("#nav-part-2 h1" , {
+    // //Parameters are optional.
+    // ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    // duration: 1,
+  });
+}
+loadingAnimation()
+cursorAnimation();
